55024cd580d8efcc097e8872112a2266333f33ef
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-25 01:38:50 +0300

    github info

--- First Week/deneme.js
+++ First Week/deneme.js
@@ -5,81 +5,70 @@ const gitHubForm = document.getElementById('gitHubForm');
 
 gitHubForm.addEventListener('submit', (e) => {
     
-
     e.preventDefault();
-
-    
     let usernameInput = document.getElementById('usernameInput');
-    //let reponameInput=document.getElementById('reponameInput');
-
-   
     let gitHubUsername = usernameInput.value;  
-    //let gitHubReponame=reponameInput.value;        
-
-    
     requestUserRepos(gitHubUsername);
+    requestUserRepos1(gitHubUsername);
 
 })
 
-
-
-
 function requestUserRepos(username){
-    
-    
-   // const xhr1 = new XMLHttpRequest();
+    const userName = document.getElementById("username");
+    const name = document.getElementById("name");
+    const profilePhoto = document.getElementById("profile-photo");
+    const repoInfo = document.querySelectorAll("#repo-info p  strong");
     const xhr = new XMLHttpRequest();
+    const url = `https://api.github.com/users/${username}`;
     
-   // const urlone=`https://api.github.com/users/${username}`;
-    const url = `https://api.github.com/users/${username}/repos?per_page=100`;
-    //https://api.github.com/users/${username}/repos?per_page=100
-   
-
     xhr.open('GET', url, true);
-   // xhr1.open('GET', urlone, true);
-    
-    
-   
+ 
     xhr.onload = function () {
     
-       
         const data = JSON.parse(this.response);
     
+        profilePhoto.src=data.avatar_url;
+        userName.innerHTML = "@" + data.login;
+        name.innerHTML = data.name;
+        repoInfo[0].innerHTML=data.public_repos;
+
+    }
+
+    xhr.send();
+
+}
+function requestUserRepos1(username){
+    const xhr1 = new XMLHttpRequest();
+    const url1 = `https://api.github.com/users/${username}/repos?per_page=100`;
+  
+    
+    xhr1.open('GET', url1, true);
  
-            
+    xhr1.onload = function () {
+    
+        const data = JSON.parse(this.response);
+        var language=new Array();
+        language=data.values;
         
-       
+    
         for (let i in data) {
-            
-            
-            const repoList=data.length;
-           
-            const repocount=data.length;
-            let ul1 = document.getElementById('userRepos');
-            var language1=new Array();
-            language1 =data[i].language;
-            //console.log(language1);
         
+            let ul1 = document.getElementById('userRepos');
+ 
           
             let li1 = document.createElement('li');
-         
-            
+
             li1.classList.add('list-group-item')
          
-            
-          
             li1.innerHTML = (`
-                <p><strong>Repo(s):</strong> ${data.length}</p> 
+                
                 <p><strong>Repository:</strong> ${data[i].name}</p>
                 <p><strong>Description:</strong> ${data[i].description}</p>
                 <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
                 <p><strong>Language:</strong> ${data[i].language}</p>
  
             `);
-       
-            
 
-   
             ul1.appendChild(li1);
          
         
@@ -88,8 +77,7 @@ function requestUserRepos(username){
     }
     
 
-    xhr.send();
+    xhr1.send();
    
     
 }
-
