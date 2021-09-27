f27f0ca16342b2fbdd7bf26006e8f3179a5f60fb
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-27 17:15:41 +0300

    reactstrap eklendi
    reactstrap eklendi

--- SecondWeek/src/App.js
+++ SecondWeek/src/App.js
@@ -2,13 +2,29 @@ import React from "react";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
+import { Container, Row ,Col} from 'reactstrap';
+
 
 function App() {
   return (
     <div >
-     <Navi></Navi>
-     <CategoryList></CategoryList>
-     <ProductList></ProductList>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+          <CategoryList></CategoryList>
+          </Col>
+          <Col xs="3">
+          <ProductList></ProductList>
+          </Col>
+          
+          
+        </Row>
+      </Container>
+
+
     </div>
   );
 }
