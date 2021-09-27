8a31ecb92ee2ff26f6422b702586302f666677b1
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-09-27 22:29:08 +0300

    Ürünler listelendi, componentler arası iletişim sağlandı
    Ürünler listelendi, componentler arası iletişim sağlandı

--- SecondWeek/src/App.js
+++ SecondWeek/src/App.js
@@ -2,20 +2,38 @@ import React, { Component } from 'react'
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
-import { Container, Row ,Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 
-export default class  App extends Component{
+export default class App extends Component {
+
+  state = { currentCategory: "", products: []};
+
   
-  state={currentCategory: "" }
+  componentDidMount(){
+    this.getProducts();
+}
 
-  changeCategory= category=>{
-    this.setState({currentCategory:category.categoryName});
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => this.setState({ products: data }));
 };
-  render (){
-    
-  let productInfo = {title: "ProductList"};
-  let categoryInfo = {title: "CategoryLiist"};
+
+  render() {
+
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
     return (
       <div >
         <Container>
@@ -24,10 +42,24 @@ export default class  App extends Component{
           </Row>
           <Row>
             <Col xs="3">
-            <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo}></CategoryList>
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+
+              >
+
+              </CategoryList>
             </Col>
             <Col xs="9">
-            <ProductList  currentCategory={this.state.currentCategory} info={productInfo}></ProductList>
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+
+              >
+
+              </ProductList>
             </Col>
           </Row>
         </Container>
--- SecondWeek/src/CategoryList.js
+++ SecondWeek/src/CategoryList.js
@@ -5,25 +5,33 @@ export default class CategoryList extends Component {
 
   
         state = {
-            categories: [
-                { categoryId: 1, categoryName: "Beverages" },
-                { categoryId: 2, categoryName: "Condiments" }
-            ]
+            categories: [ ]
         };
+
+        componentDidMount(){
+            this.getCategories();
+        }
+
+        getCategories = () => {
+            fetch("http://localhost:3000/categories ")
+            .then(response=>response.json())
+            .then(data=>this.setState({categories:data}));
+        }
     
     render() {
         return (
             <div>
                 <h3>{this.props.info.title}</h3>
+                
                 <ListGroup>
                     {
                         //map : listenın elemanlarını tek tek döner,döndüğü elemanları yenı bir liste yapar.
                         this.state.categories.map(category => (
-                            <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+                            <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.id}>{category.categoryName}</ListGroupItem>
                         ))
                     }
                 </ListGroup>
-                <h4>{this.props.currentCategory}</h4>
+                {/* <h4>{this.props.currentCategory}</h4> */}
             </div>
         )
     }
