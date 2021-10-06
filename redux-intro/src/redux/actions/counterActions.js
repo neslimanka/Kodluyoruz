7e5f95a0cc30f1a87b8d68c742e8104529c8c35b
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-10-06 22:04:13 +0300

    action yapısı çalışıldı

---
+++ redux-intro/src/redux/actions/counterActions.js
@@ -0,0 +1,17 @@
+import * as actionTypes from "./actionTypes"
+
+
+//increaseCounter bir fonskiyondur => fonksiyonun değeri{parametresi}
+//fonksiyonun değeride fonksiyon
+export const increaseCounter=()=>({
+    type:actionTypes.INCREASE_COUNTER, //fonksiyonun action tipi
+    payload:1
+})
+export const decreaseCounter=()=>({
+    type:actionTypes.DECREASE_COUNTER, //fonksiyonun action tipi
+    payload:2
+})
+export const increaseByTwoCounter=()=>({
+    type:actionTypes.INCREASE_BY_TWO_COUNTER, //fonksiyonun action tipi
+    payload:3
+})
