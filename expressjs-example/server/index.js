b85cf4504c3ca7f498d88015cb5de112048ca9e9
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-10-26 15:56:06 +0300

    nodejs ile backend oluşturuldu

--- expressjs-example/server/index.js
+++ expressjs-example/server/index.js
@@ -1,18 +1,16 @@
-// server/index.js
+const express = require("express");
 const path = require('path');
-const express = require('express');
 
-...
+const PORT = process.env.PORT || 3001;
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// Handle GET requests to /api route
+const app = express();
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server!" });
+  });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
-
-// All other GET requests not handled before will return our React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
