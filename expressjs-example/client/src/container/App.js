c09a2765c5688df3435fa1f4cfca0f103078af90
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-10-26 23:32:12 +0300

    proje herokuda canlıya alındı

---
+++ expressjs-example/client/src/container/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BillboardRow from "../components/billboard-row/BillboardRow";
+import RowContainer from "./row-container/RowContainer";
+import BillboardRow2 from "../components/billboard-row2/BillboardRow2";
+
+function App() {
+  return (
+    <>
+        <BillboardRow />
+        <RowContainer />
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <BillboardRow2 />
+        
+    </>
+  );
+}
+
+export default App;
---
+++ expressjs-example/client/src/container/row-container/RowContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Row from '../../components/row/Row';
+import styles from './styles/RowContainer.module.css'
+
+
+
+const RowContainer = () => (
+  <div className={styles.container}>
+    
+    <Row category="Cinemaximum" >Vizyonda</Row>
+  </div>
+);
+
+export default RowContainer;
